3a30b8e30200bd3d4bede826eda66f671659b571
sajan <sajanshrestharm11@gmail.com>
2020-02-27 00:49:07 -0500

    added powerball to the draw--- refactoring needs to be done

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ body = soup.tbody
 rows = body.findAll('tr', class_=None)
 
 
+# gives you the list of all numbers that was in the winning lotteries
 def get_winning_numbers():
     list_of_numbers = []
     for row in rows:
@@ -22,19 +23,36 @@ def get_winning_numbers():
             list_of_numbers.append(lt.text)
     return list_of_numbers
 
+# gives you the list of all powerballs
 
-def get_count_dictionary(winning_numbers_list):
+
+def get_all_powerballs():
+    powerballs = []
+    for row in rows:
+        data = row.find('td', class_='result')
+        ul = data.find('ul', class_='draw-result list-unstyled list-inline')
+        m_list = ul.findAll('li', class_='bonus powerball')
+        for lt in m_list:
+            pb = lt.text.split()
+            powerballs.append(pb[0])
+    return powerballs
+
+
+# gives you the dictionary of the winning number with its count
+def get_count_dictionary(number_list):
     count_of_numbers = {}
-    set_numbers = set(winning_numbers_list)
+    set_numbers = set(number_list)
     for num in set_numbers:
         count_of_numbers[num] = 0
-    for num in winning_numbers_list:
+    for num in number_list:
         count_of_numbers[num] += 1
     return count_of_numbers
 
+# gives the list of most repeated numbers
+
 
 def get_most_repeated_nums(numbers_count):
-    repeated_counts = sorted(set(sorted(numbers_count.values())))
+    repeated_counts = sorted(numbers_count.values())
     five_most_repeated_counts = repeated_counts[-5:]
     nums = []
     for key in numbers_count:
@@ -42,17 +60,36 @@ def get_most_repeated_nums(numbers_count):
             nums.append(key)
     return nums
 
+# gets most repeated powerballs
 
-def get_five_random_numbers(numbers):
+
+def get_most_repeated_powerballs(numbers_count):
+    repeated_counts = sorted(numbers_count.values())
+    most_repeated_count = repeated_counts[-1]
+    nums = []
+    for key in numbers_count:
+        if numbers_count[key] == most_repeated_count:
+            nums.append(key)
+    return nums
+
+
+# gets the draw with last number being powerball
+
+
+def get_draw(numbers):
     lottery_draw = []
     for _ in range(5):
         random_num = choice(numbers)
+        numbers.remove(random_num)
         lottery_draw.append(random_num)
+    lottery_draw = sorted(lottery_draw)
+    lottery_draw.append(choice(get_most_repeated_powerballs(
+        get_count_dictionary(get_all_powerballs()))))
     return lottery_draw
 
 
 winning_list = get_winning_numbers()
 count = get_count_dictionary(winning_list)
 numbers = get_most_repeated_nums(count)
-draw = get_five_random_numbers(numbers)
+draw = get_draw(numbers)
 print(draw)
